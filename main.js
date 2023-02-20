f99640d6de56bf5669f522cf196c4cfd711ab220
shuhsuan <shuhsuanchang.9@gmail.com>
2023-02-20 23:32:56 +0800

    functional but doesn't pass tests

--- main.js
+++ main.js
@@ -1,75 +1,116 @@
+const audioClips = [
+    {
+       keycode: 81,
+       key: "Q",
+       id:"Heater-1",
+       url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+    },
+    {
+        keycode: 87,
+        key: "W",
+        id:"Heater-2",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+    },
+    {
+        keycode: 69,
+        key: "E",
+        id:"Heater-3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+    },
+    {
+        keycode: 65,
+        key: "A",
+        id: "Heater-4",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+    },
+    {
+        keycode: 83,
+        key: "S",
+        id: "Clap",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+    },
+    {
+        keycode: 68,
+        key: "D",
+        id: "Open-HH",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+    },
+    {
+        keycode: 90,
+        key: "Z",
+        id: "Kick-n-Hat",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+    },
+    {
+        keycode: 88,
+        key: "X",
+        id: "Kick",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+    },
+    {
+        keycode: 67,
+        key: "C",
+        id: "Closed-HH",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    },
+
+    
+];
+
 function App(){
+    const [volume, setVolume] = React.useState(1);
     return(
-        <div id="drum-machine" className="text-center">
-            <div id="display">
-            <Heater1 className="drum-pad" id="Heater1">Q</Heater1>
-            <Heater2 className="drum-pad" id="Heater2">W</Heater2>
-            <Heater3 className="drum-pad" id="Heater3">E</Heater3>
-            <Heater4 className="drum-pad" id="Heater4">A</Heater4>
-            <Clap className="drum-pad" id="Clap">S</Clap>
-            <Openhh className="drum-pad" id="Openhh">D</Openhh>
-            <Kickhat className="drum-pad" id="Kickhat">Z</Kickhat>
-            <Kick className="drum-pad" id="Kick">X</Kick>
-            <Closedhh className="drum-pad" id="Closedhh">C</Closedhh>
-            </div>
-         </div>
-    )
+    <div id="drum-machine">
+     <div id = "display" className="text-center">
+        <h2>Drum Machine</h2>
+        {audioClips.map((clip)=>(
+            <Pad className="drum-pad" key={clip.id} clip={clip} volume={volume}/>
+        ))}
+        <br />
+        <h3>Volume</h3>
+        <input 
+        type="range" 
+        step="0.01"
+        onChange={(e) => setVolume(e.target.value)}
+        value={volume}
+        max="1" 
+        min="0" 
+        className="w-50"
+       /> 
+     </div>
+    </div>
+  );
 }
 
-function Heater1({handleChange}) {
-    let heater1 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3")
+function Pad({clip, volume}){
+
+    React.useEffect(() => {
+        document.addEventListener("keydown", handleKeyPress);
+        return () => {
+            document.removeEventListener("keydown", handleKeyPress);
+        };
+    }, []);
+
+    const handleKeyPress = (e) => {
+        if(e.keyCode === clip.keycode){
+            play();
+        }
+    };
+
+    const play = () => {
+        const audiobit = document.getElementById(clip.key);
+        audiobit.volume = volume;
+        audiobit.currentTime = 0;
+        audiobit.play();
+    }
+
     return(
-        <button className ="clip" id="Q" onClick={()=> heater1.play()}>Q</button>
-    )
-}
-function Heater2() {
-    let heater2 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3")
-    return(
-        <button className ="clip" id="W" onClick={()=> heater2.play()}>W</button>
-    )
-}
-function Heater3() {
-    let heater3 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3")
-    return(
-        <button className ="clip" id="E" onClick={()=> heater3.play()}>E</button>
-    )
-}
-function Heater4() {
-    let heater4 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3")
-    return(
-        <button className ="clip" id="A" onClick={()=> heater4.play()}>A</button>
-    )
-}
-function Clap() {
-    let clap = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3")
-    return(
-        <button className ="clip" id="S" onClick={()=> clap.play()}>S</button>
-    )
-}
-function Openhh() {
-    let openhh = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3")
-    return(
-        <button className ="clip" id="D" onClick={()=> openhh.play()}>D</button>
-    )
-}
-function Kickhat() {
-    let kickhat = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3")
-    return(
-        <button className ="clip" id="Z" onClick={()=> kickhat.play()}>Z</button>
-    )
-}
-function Kick() {
-    let kick = new Audio("https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3")
-    return(
-        <button className ="clip" id="X" onClick={()=> kick.play()}>X</button>
-    )
-}
-function Closedhh() {
-    let closedhh = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3")
-    return(
-        <button className ="clip" id="C" onClick={()=> closedhh.play()}>C</button>
-    )
+        <div onClick={play} className="btn btn-secondary p-4 m-3">
+            <audio className="clip" id={clip.key} src={clip.url}/>
+            {clip.key}
+        </div>
+    );
 }
 
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
